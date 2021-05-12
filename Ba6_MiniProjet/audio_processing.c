aff0614c39f0ef07af06ec6297b12683669d22c8
Astreptocoque <astreptocoque@gmail.com>
2021-05-12 20:18:37 +0200

    suppression de 2 fonctions inutiles de audio-processing (TP)

--- Ba6_MiniProjet/audio_processing.c
+++ Ba6_MiniProjet/audio_processing.c
@@ -209,14 +209,6 @@ void processAudioData(int16_t *data, uint16_t num_samples) {
 	}
 }
 
-void wait_send_to_computer(void) {
-	chBSemWait(&sendToComputer_sem);
-}
-
-float* get_audio_buffer_ptr(void) {
-	return micBack_output;
-}
-
 void microphone_start(void) {
 	sleep_mode = false;
 }
--- Ba6_MiniProjet/audio_processing.h
+++ Ba6_MiniProjet/audio_processing.h
@@ -8,7 +8,7 @@ typedef enum {
 	BACK_CMPLX_INPUT = 0,
 	//Arrays containing the computed magnitude of the complex numbers
 	BACK_OUTPUT
-} BUFFER_NAME_t;
+}BUFFER_NAME_t;
 
 typedef enum {
 	SOUND_OFF = 0,
@@ -17,16 +17,6 @@ typedef enum {
 }EARING_MODE;
 
 void processAudioData(int16_t *data, uint16_t num_samples);
-
-/*
-*	put the invoking thread into sleep until it can process the audio datas
-*/
-void wait_send_to_computer(void);
-
-/*
-*	Returns the pointer to the BUFFER_NAME_t buffer asked
-*/
-float* get_audio_buffer_ptr(void);
 void microphone_start(void);
 void microphone_stop(void);
 bool get_microphone_state(void);
