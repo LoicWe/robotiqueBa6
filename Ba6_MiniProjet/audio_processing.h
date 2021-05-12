ac30844b4b04a93d492c2c14036ea4568de5d924
Astreptocoque <astreptocoque@gmail.com>
2021-05-12 22:12:33 +0200

    modification de audio-processing.h : maj des enum

--- Ba6_MiniProjet/audio_processing.h
+++ Ba6_MiniProjet/audio_processing.h
@@ -3,22 +3,14 @@
 
 #define FFT_SIZE 	1024
 
-typedef enum {
-	//2 times FFT_SIZE because these arrays contain complex numbers (real + imaginary)
-	BACK_CMPLX_INPUT = 0,
-	//Arrays containing the computed magnitude of the complex numbers
-	BACK_OUTPUT
-}BUFFER_NAME_t;
-
-typedef enum {
+enum EARING_MODE{
 	SOUND_OFF = 0,
 	ANALYSING,
 	MOVING
-}EARING_MODE;
+};
 
 void processAudioData(int16_t *data, uint16_t num_samples);
 void microphone_start(void);
 void microphone_stop(void);
-//bool get_microphone_state(void);
 
 #endif /* AUDIO_PROCESSING_H */
